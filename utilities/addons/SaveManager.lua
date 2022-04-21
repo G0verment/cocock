3c670983825a3fcb59a6ca63393891e68fd9de64
laagginq <95953670+laagginq@users.noreply.github.com>
2022-04-20 18:35:38 -0700

    Update SaveManager.lua

--- utilities/addons/SaveManager.lua
+++ utilities/addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
+	SaveManager.Folder = 'fraudsets'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
