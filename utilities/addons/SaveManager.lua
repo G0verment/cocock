9ace7f9d34d3c7a56d3b5c405a3c5a47da0f771c
laagginq <95953670+laagginq@users.noreply.github.com>
2022-04-20 18:36:07 -0700

    Update SaveManager.lua

--- utilities/addons/SaveManager.lua
+++ utilities/addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'fraudsets'
+	SaveManager.Folder = 'fraudsaves'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
